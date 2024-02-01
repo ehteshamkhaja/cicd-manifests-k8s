9d0665f1978de6a03d462a1c6d1d0abbd4e67729
Khaja Ehteshamuddin Ahmed <ehteshamkhaja@gmail.com>
2024-02-01 21:40:33 +0300

    Update deploy-modify.sh

--- deploy-modify.sh
+++ deploy-modify.sh
@@ -4,5 +4,5 @@ cat deployment.yaml
 buildnum=${BUILD_NUMBER}
 echo "Build number old is : $buildnum"
 old_build_no=echo `(( ${buildnum} - 1 ))`
-sed -i s/"${old_build_no}/${BUILD_NUMBER}/g deployment.yaml
+sed -i "s/${old_build_no}/${BUILD_NUMBER}/g" deployment.yaml
 cat deployment.yaml
